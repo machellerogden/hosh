aca33e5e0ff0659438c8369dcb0774ec80db1f7a
Mac Heller-Ogden <mac@machellerogden.com>
2019-11-19 21:23:36 -0600

    messing around

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 #!/usr/bin/env node
-
 'use strict';
 
 if (parseInt(process.versions.node.split('.')[0]) < 12) throw new Error('Hosh requires Node.js v12 or newer.');
 
-const Vorpal = require('vorpal');
-const PDN = require('pdn');
-const vorpal = Vorpal();
+if (!process.stdin.isTTY) {
+    console.error('hosh can only run as TTY for the time being.');
+    process.exit(1);
+}
 
-vorpal
-    .command('echo [input...]', 'echo given args')
-    .action(function (args, callback) {
-        this.log(args.input);
-        callback();
-    });
+process.stdin.setRawMode(true);
 
-vorpal
-    .delimiter('@>')
-    .show();
+const os = require('os');
+const pty = require('node-pty');
+const { columns:cols, rows } = require('term-size');
+
+const ptyProcess = pty.spawn('bash', [ '-l' ], {
+    name: 'xterm-color',
+    cols,
+    rows,
+    cwd: process.env.HOME,
+    env: process.env
+});
+
+ptyProcess.pipe(process.stdout);
+process.stdin.pipe(ptyProcess);
+
+//const Vorpal = require('vorpal');
+//const PDN = require('pdn');
+//const vorpal = Vorpal();
+
+//vorpal
+    //.command('echo [input...]', 'echo given args')
+    //.action(function (args, callback) {
+        //this.log(args.input);
+        //callback();
+    //});
+//vorpal
+    //.delimiter('@>')
+    //.show();
